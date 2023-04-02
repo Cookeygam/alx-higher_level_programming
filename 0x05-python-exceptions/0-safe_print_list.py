e9567cab22c2c4d2e35db3ab90e55238c6424bee
cookeygam <114502105+Cookeygam@users.noreply.github.com>
2023-04-02 21:23:12 +0100

    Update 0-safe_print_list.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
-def safe_print_integer(value):
-    try:
-        print("{:d}".format(value))
-    except(ValueError, TypeError):
-        return False
-    else:
-        return True
+def safe_print_list(my_list=[], x=0):
+    n = 0
+    for m in range(x):
+        try:
+            print(my_list[m], end="")
+            n += 1
+        except IndexError:
+            break
+    print()
+    return n
